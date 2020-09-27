2878e568f98a247a8c7166bad40ea18dd0c8c1f9
CGsama <CGsama@outlook.com>
2020-09-27 18:10:22 -0400

    Update bk.js

--- bk.js
+++ bk.js
@@ -47,7 +47,7 @@ function nextPage(){
 				str.match(/title="(20\d+)年(\d+)月(\d+)日/g).forEach(function(datestr){
 					match = datestr.match(/title="(20\d+)年(\d+)月(\d+)日/);
 					datestr = match[1] + ("000" + match[2]).slice(-2) + match[3];
-					if(parseInt(date) < parseInt(datestr)){
+					if(parseInt(date) > parseInt(datestr)){
 						date = datestr;
 					}
 				});
