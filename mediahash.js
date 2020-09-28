2697406d9ab544a60bca0972db02ec0c8e188b33
CGsama <CGsama@outlook.com>
2020-09-28 05:34:43 -0400

    Update mediahash.js

--- mediahash.js
+++ mediahash.js
@@ -1,22 +1,37 @@
 var fs = require('fs');
 
-str = fs.readFileSync("txwb.bak").toString();
+str = fs.readFileSync(process.argv[2]).toString();
+str.match(/http:\/\/mblogpic.store.qq.com\/mblogpic\/(\w+)\//g).forEach(function(url){
+	match = url.match(/http:\/\/mblogpic.store.qq.com\/mblogpic\/(\w+)\//);
+	fs.appendFileSync("media.store.hash", match[1] + "\n");
+});
+str.match(/http:\/\/t0.qpic.cn\/mblogpic\/(\w+)\//g).forEach(function(url){
+	match = url.match(/http:\/\/t0.qpic.cn\/mblogpic\/(\w+)\//);
+	fs.appendFileSync("media.t0.hash", match[1] + "\n");
+});
 str.match(/http:\/\/t1.qpic.cn\/mblogpic\/(\w+)\//g).forEach(function(url){
 	match = url.match(/http:\/\/t1.qpic.cn\/mblogpic\/(\w+)\//);
-	fs.appendFileSync("media.hash", match[1] + "\n");
+	fs.appendFileSync("media.t1.hash", match[1] + "\n");
+});
+str.match(/http:\/\/t2.qpic.cn\/mblogpic\/(\w+)\//g).forEach(function(url){
+	match = url.match(/http:\/\/t2.qpic.cn\/mblogpic\/(\w+)\//);
+	fs.appendFileSync("media.t2.hash", match[1] + "\n");
 });
 
-mb = fs.readFileSync("media.hash").toString().split(/\r?\n/);
-arr = [];
+hosts=["store", "t0", "t1", "t2"];
+hosts.forEach(function(host){
+	mb = fs.readFileSync("media." + host + ".hash").toString().split(/\r?\n/);
+	arr = [];
 
-mb.forEach(function(line){
-	if(! arr.includes(line)){
-		arr.push(line);
-	}
-});
+	mb.forEach(function(line){
+		if(! arr.includes(line)){
+			arr.push(line);
+		}
+	});
 
-fs.writeFileSync("media.hash", "");
+	fs.writeFileSync("media." + host + ".hash", "");
 
-arr.forEach(function(line){
-	fs.appendFileSync("media.hash", line + "\n");
+	arr.forEach(function(line){
+		fs.appendFileSync("media." + host + ".hash", line + "\n");
+	});
 });
